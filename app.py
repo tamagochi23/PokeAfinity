77ffd66f6ae1656c1f7423965ca43d24a95d9278
DavidMonsalves98 <davidmonsalves98@gmail.com>
2025-03-23 18:35:20 -0300

    first commit

---
+++ app.py
@@ -0,0 +1,16 @@
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+@app.route("/", methods = ["GET", "POST"])  # Define la ruta principal
+def index():
+    if request.method == "POST":
+        data = dict()
+        data["nombre"] = request.form["nombre"]  # Obtiene el valor del campo nombre
+        data["edad"] = request.form["edad"] # Obtiene el valor del campo edad
+        print(data)
+        return render_template("index.html", data = data) 
+    else:
+        return render_template("index.html")
+if __name__ == "__main__":
+    app.run(debug=True)  # Inicia el servidor en modo de desarrollo
