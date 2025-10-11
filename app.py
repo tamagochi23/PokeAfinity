39812d2acd5fef7f163455e4f744575f8bd8d2d9
DavidMonsalves98 <david.tamagochi23@hotmail.com>
2025-10-11 15:00:14 -0300

    use redirect

--- app.py
+++ app.py
@@ -9,8 +9,12 @@ def index():
         data["nombre"] = request.form["nombre"]  # Obtiene el valor del campo nombre
         data["edad"] = request.form["edad"] # Obtiene el valor del campo edad
         print(data)
-        return render_template("index.html", data = data) 
+        return redirect("/resultados", data = data) 
     else:
         return render_template("index.html")
+@app.route("/resultados")
+def resultados(data):
+    print(data)
+    return render_template("templates/index.html")
 if __name__ == "__main__":
     app.run(debug=True)  # Inicia el servidor en modo de desarrollo
